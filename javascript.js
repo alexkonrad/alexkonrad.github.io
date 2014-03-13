2f9b620324af8ee2454ad1e9dfbbd9f9b2cfc8a1
Alex Konrad <alexkonrad08@gmail.com>
2014-03-13 15:51:56 -0400

    change layout to parallax

--- javascript.js
+++ javascript.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
-  $('.projects').on('click', function(event) {
-    event.preventDefault();
-      
-  })
+  $(window).bind('scroll',function(e){
+    parallaxScroll();
+  });
+ 
+  function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
+    $('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
+    $('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
+  };
+  
+  $('a.sidebar-link').click(function(){
+  	$('html, body').animate({
+  		scrollTop:$('#sidebar-link').offset().top
+  	}, 1000, function() {
+    	parallaxScroll(); // Callback is required for iOS
+	});
+  	return false;
+  });
 })

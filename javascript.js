12f692722048cd2c402a02f877d4ac11d5373e2b
Alex Konrad <alexkonrad08@gmail.com>
2014-03-14 16:39:35 -0400

    add sidebar

--- javascript.js
+++ javascript.js
@@ -5,17 +5,41 @@ $(document).ready(function() {
  
   function parallaxScroll(){
     var scrolled = $(window).scrollTop();
-    $('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
-    $('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
-    $('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
+    $('#parallax-bg1').css('top',(0-(scrolled*.35))+'px');
+    $('#parallax-bg2').css('top',(1000-(scrolled*.75))+'px');
+    $('#parallax-bg3').css('top',(2000-(scrolled*.75))+'px');
   };
   
-  $('a.sidebar-link').click(function(){
+  $('a#name-link').click(function(){
   	$('html, body').animate({
-  		scrollTop:$('#sidebar-link').offset().top
+  		scrollTop:$('#name').offset().top
   	}, 1000, function() {
     	parallaxScroll(); // Callback is required for iOS
 	});
   	return false;
   });
+  $('a#bio-link').click(function(){
+  	$('html, body').animate({
+  		scrollTop:$('#bio').offset().top
+  	}, 1000, function() {
+    	parallaxScroll(); // Callback is required for iOS
+	});
+  	return false;
+  });
+  $('a#projects-link').click(function(){
+  	$('html, body').animate({
+  		scrollTop:$('#projects').offset().top
+  	}, 1000, function() {
+    	parallaxScroll(); // Callback is required for iOS
+	});
+  	return false;
+  });
+  $('a#about-link').click(function(){
+  	$('html, body').animate({
+  		scrollTop:$('#about').offset().top
+  	}, 1000, function() {
+    	parallaxScroll(); // Callback is required for iOS
+	});
+  	return false;
+  });  
 })

4a213ab89fbfaefd4e61e655cddbd39dc225ce24
Alex Konrad <alexkonrad08@gmail.com>
2014-03-18 14:25:18 -0400

    add about page

--- javascript.js
+++ javascript.js
@@ -2,26 +2,26 @@ $(document).ready(function() {
   $(window).bind('scroll',function(e){
     parallaxScroll();
   });
- 
+
   function parallaxScroll(){
     var scrolled = $(window).scrollTop();
     $('#parallax-bg1').css('top',(0-(scrolled*.75))+'px');
     $('#parallax-bg2').css('top',(1000-(scrolled*1.25))+'px');
     $('#parallax-bg3').css('top',(2000-(scrolled*.25))+'px');
-    
+
     $('li').removeClass('active')
-    console.log(scrolled)
+
     if (scrolled < 1000) {
       $('#li-name-link').addClass('active');
     } else if (scrolled > 1000 && scrolled < 2000 ) {
-      $('#li-bio-link').addClass('active');      
+      $('#li-bio-link').addClass('active');
     } else if (scrolled > 2000 && scrolled < 3000) {
-      $('#li-projects-link').addClass('active');      
+      $('#li-projects-link').addClass('active');
     } else if (scrolled > 3000 && scrolled < 4000) {
-      $('#li-about-link').addClass('active');      
+      $('#li-about-link').addClass('active');
     }
   };
-  
+
   $('a#name-link').click(function(){
   	$('html, body').animate({
   		scrollTop:$('#name').offset().top
@@ -53,5 +53,5 @@ $(document).ready(function() {
     	parallaxScroll(); // Callback is required for iOS
 	});
   	return false;
-  });  
+  });
 })
